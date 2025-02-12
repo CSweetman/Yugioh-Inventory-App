a7309b34d18130d929bf18cd4f1eaafe2bee873a
CSweetman <chrisjdsweetman@gmail.com>
2025-02-12 13:16:24 -0600

    Dashboard Page progress: Popuplar products and sales summary component

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,61 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export interface Product {
+    productId: string
+    name: string
+    price: number
+    rating?: number
+    stockQuantity: number
+}
+
+export interface SalesSummary {
+    salesSummaryId: string
+    totalValue: number
+    changePercentage?: number
+    date: string
+}
+
+export interface PurchaseSummary {
+    purchaseSummaryId: string
+    totalPurchased: number
+    changePercentage?: number
+    date: string
+}
+
+export interface ExpenseSummary {
+    expenseSummaryId: string
+    totalExpenses: number
+    date: string
+}
+
+export interface ExpenseByCategorySummary {
+    expenseByCategorySummaryId: string
+    category: string
+    amount: string
+    date: string
+}
+
+export interface DashboardMetrics {
+    popularProducts: Product[]
+    salesSummary: SalesSummary[]
+    purchaseSummary: PurchaseSummary[]
+    expenseSummary: ExpenseSummary[]
+    expenseByCategorySummary: ExpenseByCategorySummary[]
+}
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
     reducerPath: "api",
-    tagTypes: [],
-    endpoints: (build) => ({}),
+    tagTypes: ["DashboardMetrics"],
+    endpoints: (build) => ({
+        // 1st param is datatype receiving, 2nd param is datatype that is being sent
+        // Tags help us declare when the data is stale/invalidated and needs to be refetched
+        // This will all be stored in the redux store
+        getDashboardMetrics: build.query<DashboardMetrics, void>({
+            query: () => "/dashboard",
+            providesTags: ["DashboardMetrics"],
+        }),
+    }),
 })
 
-export const { } = api;
+export const { useGetDashboardMetricsQuery } = api
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import helmet from 'helmet'
-import morgan from 'morgan'
+import express from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import dashboardRoutes from "./routes/dashboardRoutes"
 
 // Route Imports
 
@@ -13,12 +14,16 @@ const app = express()
 app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
-app.use(morgan('common'))
+app.use(morgan("common"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
 // Routes
+app.use('/dashboard', dashboardRoutes)
+// app.get("/hello", (req, res) => {
+//     res.send("hello world2")
+// })
 
 // Server
 const port = process.env.PORT || 3001
